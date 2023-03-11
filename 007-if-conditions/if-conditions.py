63c7d3c4278af5961c4850b1e8ac9d9ed5225aa8
saifmohamedsv <xcyberghostx1@gmail.com>
2023-03-11 16:35:53 +0200

    If statements

---
+++ 007-if-conditions/if-conditions.py
@@ -0,0 +1,14 @@
+# If Statements = a block of code will execute only if it's condition is true
+
+age = int(input("Enter your age: "))  # Collect age as integer from the user
+
+if age >= 18:  # Check if the age is bigger than or equal 25
+    # If and only if the age is more than or equal 25 the print will run
+    print("You're an adult!")
+
+elif age == 15:  # if the first condition is not true will check this one after...
+    print("You're 15!")
+
+else:  # This will be checked at the end, after all the `elif` statements
+    print("You still a kid!")
+ 
